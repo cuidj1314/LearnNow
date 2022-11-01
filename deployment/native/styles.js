d4120304e31bc215a4a5e459347e9b726c2921f9
cuidj <cuidj1314@gmail.com>
2022-11-01 22:54:41 +0800

    add

---
+++ deployment/native/styles.js
@@ -0,0 +1,10 @@
+import * as style0 from "D:/workspace/Mendix/LearnNow/themesource/administration/native/main";
+import * as style1 from "D:/workspace/Mendix/LearnNow/themesource/nanoflowcommons/native/main";
+import * as style2 from "D:/workspace/Mendix/LearnNow/themesource/webactions/native/main";
+import * as style3 from "D:/workspace/Mendix/LearnNow/themesource/atlas_core/native/main";
+import * as style4 from "D:/workspace/Mendix/LearnNow/themesource/myfirstmodule/native/main";
+import * as style5 from "D:/workspace/Mendix/LearnNow/theme/native/main";
+
+import { flatten } from "mendix/native";
+
+module.exports = flatten([style0, style1, style2, style3, style4, style5]);
---
+++ deployment/react-web/start.bat
@@ -0,0 +1,1 @@
+"E:/Program Files/Mendix/9.18.1.54902\modeler\tools\node\node.exe" "E:/Program Files/Mendix/9.18.1.54902\modeler\tools\node\node_modules\rollup\dist\bin\rollup" -c -w
